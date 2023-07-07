62158c6310116d8cfc27e952a1adefb5b7a820a5
Khafizi Noh <khafizi.noh@gmail.com>
2023-07-08 00:17:58 +0800

    Include IIFE in `app.js`

--- src/js/app.js
+++ src/js/app.js
@@ -1,99 +1,109 @@
 /**
  * --------------------------------------------------------------------------
- * Style Guideline: app.js
+ * Style Guideline v0.2.0: app.js
  * Licensed under MIT (https://github.com/mkfizi/style-guideline/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
 
-'use strict';
+(function () {
+    'use strict';
 
-let app = {
-    name: 'Style Guideline',
-    version: '0.2.0',
-};
+    const app = {
+        name: 'Style Guideline',
+        version:'0.2.0',
+        elements: {
+            id: {
+                darkMode: {
+                    toggle: "dark-mode-toggle"
+                }
+            },
+            node: {
+                footer: {
+                    year: document.getElementById('footer-year'),
+                    appName: document.getElementById('footer-app-name'),
+                    appVersion: document.getElementById('footer-app-version'),
+                }
+            }
+        },
 
-app.elements = {
-    darkModeToggle: document.getElementById('dark-mode-toggle'),
-    footerYear: document.getElementById('footer-year'),
-    footerAppName: document.getElementById('footer-app-name'),
-    footerAppVersion: document.getElementById('footer-app-version'),
-};
+        events: {
+            document: {
+                click: event => {
+                    const target = event.target;
+                    if (target.closest(`[id=${app.elements.id.darkMode.toggle}]`)) {
+                        app.views.darkMode.toggle();
+                    }
+                }
+            },
 
-app.init = () => {
-    app.view.init();
-    app.event.init();
-};
+            window: {
+                resize: () => {
+                    app.views.viewportHeight.toggle();
+                }
+            },
 
-app.event = {
-    init: () => {
-        document.addEventListener('click', app.event.handleDocumentClick);
-        window.addEventListener('resize', app.event.handleWindowResize);
-    },
+            init: () => {
+                document.addEventListener('click', app.events.document.click);
+                window.addEventListener('resize', app.events.window.resize);
+            }
+        },
 
-    handleDocumentClick: event => {
-        const target = event.target;
+        views: {
+            viewportHeight: {
+                toggle: () => {
+                    document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
+                }
+            },
 
-        if (target.closest('[id="dark-mode-toggle"]')) {
-            app.view.darkMode.toggle();
+            darkMode: {
+                toggle: () => {
+                    app.utils.transition.toggle();
+        
+                    const isDarkMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
+                    localStorage.theme = isDarkMode ? 'dark' : 'light';
+                    document.documentElement.classList.toggle('dark', isDarkMode);
+                }
+            },
+
+            footer: {
+                toggle: () => {
+                    if (app.elements.node.footer.year) {
+                        app.elements.node.footer.year.innerHTML = new Date().getFullYear();
+                    }
+
+                    if (app.elements.node.footer.appName) {
+                        app.elements.node.footer.appName.innerHTML = app.name;
+                    }
+                    if (app.elements.node.footer.appVersion) {
+                        app.elements.node.footer.appVersion.innerHTML = app.version;
+                    }
+                }
+            },
+
+            init: () => {
+                app.views.footer.toggle();
+            }
+        },
+
+        utils: {
+            transition: {
+                toggle: () => {
+                    const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
+                    for (const transition of transitions) {
+                        transition.classList.add('transition-none');
+                        setTimeout(() => {
+                            transition.classList.remove('transition-none');
+                        }, 100);
+                    }
+                }
+            }
+        },
+
+        init: () => {
+            app.events.init();
+            app.views.init();
         }
-    },
-
-    handleWindowResize: () => {
-        app.view.viewportHeight.toggle();
     }
-};
 
-app.view = {
-    init: () => {
-        app.view.viewportHeight.toggle();
-        app.view.footer.toggle();
-    },
-
-    viewportHeight: {
-        toggle: () => {
-            document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
-        }
-    },
-
-    darkMode: {
-        toggle: () => {
-            app.util.transition.toggle();
-
-            const isLightMode = localStorage.theme === 'light' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: light)').matches);
-            localStorage.theme = isLightMode ? 'dark' : 'light';
-            document.documentElement.classList.toggle('dark', isLightMode);
-        }
-    },
-
-    footer: {
-        toggle: () => {
-            if (app.elements.footerYear) {
-                app.elements.footerYear.innerHTML = new Date().getFullYear();
-            }
-    
-            if (app.elements.footerAppName) {
-                app.elements.footerAppName.innerHTML = app.name;
-            }
-            
-            if (app.elements.footerAppVersion) {
-                app.elements.footerAppVersion.innerHTML = app.version;
-            }
-        }
-    }
-};
-
-app.util = {
-    transition: {
-        toggle: () => {
-            const transitions = document.querySelectorAll('.transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform');
-            for (const transition of transitions) {
-                transition.classList.add('transition-none');
-                setTimeout(() => {
-                    transition.classList.remove('transition-none');
-                }, 100);
-            }
-        }
-    }
-};
-
-app.init();
+    app.init();
+})();
