a0eb73f8dbb285c398d5ddbd83ff1d51d018c5ef
Khafizi Noh <khafizi.noh@gmail.com>
2023-11-03 21:01:49 +0800

    Update version no

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@
     const app = {};
 
     app.name = 'Style Guide';
-    app.version = '0.3.1';
+    app.version = '0.3.2';
 
     app.element = {
         navbar: document.getElementById('navbar'),
