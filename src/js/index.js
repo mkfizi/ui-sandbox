65f981e30ad16d0094b1ad5a1c890a348a21d6c8
Khafizi Noh <khafizi.noh@gmail.com>
2024-05-03 15:44:42 +0800

    Remove comment from `index.js`

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,3 @@
-/**
- * --------------------------------------------------------------------------
- * Style Guide v0.4.1: app.js
- * Licensed under MIT (https://github.com/mkfizi/styleguide/blob/main/LICENSE)
- * --------------------------------------------------------------------------
- */
-
 'use strict';
 
 import Alpine from 'alpinejs';
