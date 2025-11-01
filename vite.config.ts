387adff7e8986d185c3edea10e08db2096d1b92a
Khafizi Noh <khafizi.noh@gmail.com>
2025-11-01 12:39:46 +0800

    Rename `vite-plugin-shiki` to `shiki.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 import { glob } from 'glob';
-import vitePluginShiki from './vite.plugins/vite-plugin-shiki.ts';
+import vitePluginShiki from './vite.plugins/shiki.ts';
 
 export default defineConfig({
     base: '/ui-sandbox/',
