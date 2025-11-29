ff2188a1e030b36581d1b6783e062ad3cb42402a
Khafizi Noh <khafizi.noh@gmail.com>
2025-11-29 18:44:28 +0800

    Move `dark:` classes to front, Add `pointer-events-none`

--- vite.plugins/shiki.ts
+++ vite.plugins/shiki.ts
@@ -55,10 +55,10 @@ const utility: object = {
                 aria-label="Copy code."
                 data-code="${code.replace(/"/g, '&quot;').replace(/'/g, '&#39;')}"
             >
-                <span x-show="!isCopied" class="[&>svg]:stroke-black dark:[&>svg]:stroke-white [&>svg]:w-5 [&>svg]:h-5 shrink-0">
+                <span x-show="!isCopied" class="[&_svg]:stroke-black dark:[&_svg]:stroke-white [&_svg]:w-5 [&_svg]:h-5 shrink-0">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"/><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"/></svg>
                 </span>
-                <span x-show="isCopied" x-cloak class="[&>svg]:stroke-black dark:[&>svg]:stroke-white [&>svg]:w-5 [&>svg]:h-5 shrink-0">
+                <span x-show="isCopied" x-cloak class="[&_svg]:stroke-black dark:[&_svg]:stroke-white [&_svg]:w-5 [&_svg]:h-5 shrink-0">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"/><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"/><path d="M9 14l2 2l4 -4"/></svg>
                 </span>
             </button>
